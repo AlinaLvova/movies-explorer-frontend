e607f8b27a05ef43cf11556df6a30dfe66c37238
Lvova Alina <lvova.aline@gmail.com>
2023-06-29 15:33:32 +0400

    feature: make the section heading a separate element

--- movie-explorer/src/components/Main/AboutProject/AboutProject.js
+++ movie-explorer/src/components/Main/AboutProject/AboutProject.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 
 import "./AboutProject.css";
+import SectionHeader from "../../Common/SectionHeader/SectionHeader";
 
 function AboutProject() {
   return (
     <section className="about-project">
-      <div className="about-project__title">О проекте</div>
+      <SectionHeader 
+      headerTitle={"О проекте"}/>
       <ul className="about-project__table">
-        <li class="about-project__container">
+        <li className="about-project__container">
           <div className="about-project__subtitle">Дипломный проект включал 5 этапов</div>
           <div className="about-project__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</div>
         </li>
@@ -19,7 +21,7 @@ function AboutProject() {
       <div className="about-project__timeline-container">
         <div className="about-project__week-container">
           <div className="about-project__timeline-bar about-project__timeline-bar_green">1 неделя</div>
-          <div class="about-project__caption">Back-end</div>
+          <div className="about-project__caption">Back-end</div>
         </div>
         <div className="about-project__week-container">
           <div className="about-project__timeline-bar about-project__timeline-bar_grey">4 недели</div>
