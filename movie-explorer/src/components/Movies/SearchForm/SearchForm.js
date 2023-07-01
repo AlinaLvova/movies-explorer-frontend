247ccd5b9e5d09f4c1358282df45fa5df4a92f0a
Lvova Alina <lvova.aline@gmail.com>
2023-07-01 23:25:56 +0400

    feature: add search form sector

--- movie-explorer/src/components/Movies/SearchForm/SearchForm.js
+++ movie-explorer/src/components/Movies/SearchForm/SearchForm.js
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './SearchForm.css';
+import "./SearchForm.css";
 
-function SearchForm () {
+function SearchForm() {
   const [activeSwitcher, setActiveSwitcher] = useState(false);
 
   function handleSwitcherClick() {
     setActiveSwitcher(!activeSwitcher);
-  };
+  }
 
   return (
     <section className="search-form">
-    <div className="search-form__container">
-      <div className="search-form__search-icon"></div>
-      <input className="search-form__input-field" type="text" placeholder="Фильм"></input>
-      <button className="search-form__search-btn link" type="button">Найти</button>
-    </div>
-    <div className="search-form__delimeter"></div>
-    <div className="search-form__switcher-container">
-      <button className={`search-form__switcher ${activeSwitcher ? "active" : ""}`} onClick={handleSwitcherClick}>
-        <div className="search-form__switcher-ball"></div>
-      </button>
-      <div className="search-form__switcher-text">Короткометражка</div>
-    </div>
+      <div className="search-form__container">
+        <div className="search-form__search-icon"></div>
+        <input
+          className="search-form__input-field"
+          type="text"
+          placeholder="Фильм"
+        ></input>
+        <button className="search-form__search-btn link" type="button">
+          Найти
+        </button>
+      </div>
+      <div className="search-form__delimeter"></div>
+      <div className="search-form__switcher-container">
+        <button
+          className={`search-form__switcher ${activeSwitcher ? "active" : ""}`}
+          onClick={handleSwitcherClick}
+        >
+          <div className="search-form__switcher-ball"></div>
+        </button>
+        <div className="search-form__switcher-text">Короткометражка</div>
+      </div>
     </section>
   );
 }
