8833009a47e98bdc19de69e0f8871713b47a26c4
Lvova Alina <lvova.aline@gmail.com>
2023-07-16 18:35:45 +0400

    feature: add mainApi

---
+++ movie-explorer/src/utils/MainApi.js
@@ -0,0 +1,111 @@
+import { BASE_URL } from '../utils/constant';
+
+class Api{
+    constructor(config){
+        this.baseUrl = config.baseUrl;
+        this.headers = config.headers;
+    }
+
+    // функция обработки результата ответа сервера
+    _handleResponse (response, errorMessage) {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(new Error(`Ошибка: ${response.status}. Сообщение об ошибке: ${errorMessage}`));
+    };
+
+    // Сохранить фильм в избранное
+    saveMovie(movie){
+        return fetch(`${this.baseUrl}/movies`, {
+            headers: this.headers,
+            method: 'POST',
+            body: JSON.stringify({movie})
+        })
+        .then((response) => {
+            return this._handleResponse(response, "Данные о фильме не были успешно получены сервером")
+        });
+    }
+    
+    //удалить фильм по id
+    deleteMovie(movieId) {
+        return fetch(`${this.baseUrl}/movies/${movieId}`, {
+            headers: this.headers,
+            method: 'DELETE'
+        })
+        .then((response) => {
+            return this._handleResponse(response, "Данные о фильме не были успешно удалены на сервере")
+        });
+    }
+
+    //получить список фильмов
+    getMovieList() {
+        return fetch(`${this.baseUrl}/movies`, {
+            headers: this.headers,
+            method: 'GET'
+        })
+        .then((response) => {
+            return this._handleResponse(response, "Данные о списке фильмов не были успешно получены")
+        });    
+    }
+
+    //получить информацию о пользователе
+    getUserInfo(){
+        return fetch(`${this.baseUrl}/users/me`, {
+            headers: this.headers,
+            method: 'GET'
+        })
+        .then((response) => {
+            return this._handleResponse(response, "Данные о пользователе не были успешно получены")
+        });
+    }
+
+    //обновление данных о пользователе на сервере
+    updateUserInfo(name, email){
+        return fetch(`${this.baseUrl}/users/me`, {
+            headers: this.headers,
+            method: 'PATCH',
+            body: JSON.stringify({
+                name: name,
+                email: email
+            })
+        })
+        .then((response) => {
+            return this._handleResponse(response, "Данные о пользователе не были успешно обновлены на сервере")
+        });
+    }
+
+    // Авторизироваться
+    login(userData){
+        return fetch(`${this.baseUrl}/signin`, {
+            headers: this.headers,
+            method: 'POST',
+            body: JSON.stringify(userData)
+        })
+        .then((response) => {
+            return this._handleResponse(response, "Не удалось успешно авторизоваться")
+        });
+    }
+
+    // Зарегистрироваться
+    register(userData){
+        return fetch(`${this.baseUrl}/signup`, {
+            headers: this.headers,
+            method: 'POST',
+            body: JSON.stringify(userData)
+        })
+        .then((response) => {
+            return this._handleResponse(response, "Не удалось успешно зарегистрироваться")
+        });
+    }
+}   
+
+const apiConfig = {
+    baseUrl: BASE_URL,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+const api = new Api(apiConfig);
+
+export default api;
--- movie-explorer/src/utils/constant.js
+++ movie-explorer/src/utils/constant.js
@@ -1,5 +1,7 @@
 const MENU_ICON_SCREEN_WIDTH = 769;
 
+const BASE_URL = 'https://api.movie.nomoredomains.rocks';
+
 module.exports = {
   MENU_ICON_SCREEN_WIDTH,
 };
