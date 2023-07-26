e5a09ea52a77c627313ef71bc588b07c2708bb5c
Lvova Alina <lvova.aline@gmail.com>
2023-07-27 02:24:16 +0400

    fix: add back button functionality to "Not Found" page.

--- movie-explorer/src/components/NotFound/NotFound.js
+++ movie-explorer/src/components/NotFound/NotFound.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./NotFound.css";
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); // Перенаправляем на предыдущую страницу
+  };
   return (
     <div className="page__container">
       <main className="content content_not-found">
         <section className="not-found">
           <h1 className="not-found__header">404</h1>
           <p className="not-found__subtitle">Страница не найдена</p>
-          <Link to="/" className="not-found__back-link link">
+          <Link to="#" className="not-found__back-link link" onClick={handleGoBack}>
             Назад
           </Link>
         </section>
